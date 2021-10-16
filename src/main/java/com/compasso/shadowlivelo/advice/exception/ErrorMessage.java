67aa60e54579257c218c026d5bb615733fb551d3
yasminwz <yas.desenv@gmail.com>
2021-10-15 22:58:34 -0300

    Adicionando ErrorMessage

---
+++ src/main/java/com/compasso/shadowlivelo/advice/exception/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.compasso.shadowlivelo.advice.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+
+    private String error;
+    private String details;
+
+}
